73d2f0e254aa43214831879d0674990874c6afb2
NatVas <vas.nat.al@yandex.ru>
2019-11-12 15:53:16 +0300

    1
    загрузка проекта. плохое качество, плохая обработка зеленого

---
+++ homework_1_image_an/homework_1_image_an.cpp
@@ -0,0 +1,14 @@
+﻿#include "my_image.h"
+#include <fstream>
+#include <iostream>
+#include <Windows.h>
+
+
+int main()
+{
+	const char* file_name = "RGB_CFA.bmp";
+	const char* file_name_load = "5_6.bmp";
+	my_image BPM_IMAGE(file_name);
+	BPM_IMAGE.Make_better();
+	BPM_IMAGE.Load_to_file(file_name_load);
+}
